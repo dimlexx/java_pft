533df169b55ad936686d71b58a2fd26271fc4970
dimlexx <78341464+dimlexx@users.noreply.github.com>
2021-02-01 19:27:30 +0700

    математические операции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -3,11 +3,5 @@ package ru.stqa.pft.sandbox;
 public class MFP {
   public static void main(String[] args) {
     System.out.println("Hello !0!0!");
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
-    System.out.println(1/3);
-    System.out.println(1.0/2);
-  }
+   }
 }
