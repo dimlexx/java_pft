baef10f2013f78456a0cea216461a1fb5058c904
dimlexx <78341464+dimlexx@users.noreply.github.com>
2021-02-02 12:55:20 +0700

    переменные и значения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MFP {
   public static void main(String[] args) {
-    System.out.println("Hello !0!0!");
-   }
+    System.out.println("Hello !");
+    int l = 8;
+    int s = l * l;
+    System.out.println("Площадь квадрата " + l + " = " + s);
+  }
 }
