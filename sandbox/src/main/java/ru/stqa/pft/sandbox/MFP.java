58837b410fa05ec2cbe553fea5dfb04c8ee472a1
dimlexx <78341464+dimlexx@users.noreply.github.com>
2021-02-02 14:16:45 +0700

    New function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -2,14 +2,35 @@ package ru.stqa.pft.sandbox;
 
 public class MFP {
   public static void main(String[] args) {
-    String somebody = "world";
-    System.out.println("Hello " + somebody + " !");
-    int l = 8;
+    hello("world");
+    hello("123");
+    hello("qwe");
+
+    double len = 5;
+    System.out.println("Площадь " + len + " = " + area(len));
+
+    int l = 15;
     int s = l * l;
     System.out.println("Площадь квадрата " + l + " = " + s);
-
-    double q = 8;
+    double q = 2;
     double w = q * q;
     System.out.println("Площадь квадрата " + q + " = " + w);
+
+double a=4;
+double b = 6;
+System.out.println("Площадь прямоугольника " + a + " и " + b + " = " + area(a, b));
   }
+
+    public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + " !");
+  }
+
+  public static double area (double qq){
+    return qq * qq;
+  }
+
+  public static double area (double a, double b) {
+  return a * b;
+  }
+
 }
