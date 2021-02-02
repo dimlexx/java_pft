6b6e941467f647bc43ce59ee1bae54241ea6b3ca
dimlexx <78341464+dimlexx@users.noreply.github.com>
2021-02-02 13:05:32 +0700

    Дробные и целые + переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -2,9 +2,14 @@ package ru.stqa.pft.sandbox;
 
 public class MFP {
   public static void main(String[] args) {
-    System.out.println("Hello !");
+    String somebody = "world";
+    System.out.println("Hello " + somebody + " !");
     int l = 8;
     int s = l * l;
     System.out.println("Площадь квадрата " + l + " = " + s);
+
+    double q = 8;
+    double w = q * q;
+    System.out.println("Площадь квадрата " + q + " = " + w);
   }
 }
